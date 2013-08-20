d0b8dd95c01f44d147509c45b4d32fec9a33d77a
uldisn <uldisnelsons@gmail.com>
2013-08-20 20:00:22 +0300

    pielaboju tulkoshanu

--- translation.php
+++ translation.php
@@ -1,8 +1,9 @@
 <?php
+//yiic message ../vendor/dbrisinajumi/d1status/translation.php
 return array(
-    'sourcePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',  //root dir of all source
+    'sourcePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR ,  //root dir of all source
     'messagePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR .'messages',  //root dir of message translations
-    'languages'  => array('lv',),  //array of lang codes to translate to, e.g. es_mx
+    'languages'  => array('lv','en','ru'),  //array of lang codes to translate to, e.g. es_mx
     'fileTypes' => array('php',), //array of extensions no dot all others excluded
     //'exclude' => array('.svn',),  //list of paths or files to exclude
     'translator' => 'Yii::t',  //this is the default but lets be complete
