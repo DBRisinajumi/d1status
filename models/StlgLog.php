fed74f54f6af37141d21d2b5f491ba87c2cd17c0
uldisn <uldisnelsons@gmail.com>
2013-10-24 14:38:34 +0300

    normalizets auditrail

--- models/StlgLog.php
+++ models/StlgLog.php
@@ -28,7 +28,7 @@ class StlgLog extends BaseStlgLog
                 parent::behaviors(),
                 array(
                     'LoggableBehavior'=>array(
-                        'class' => 'vendor.sammaye.auditrail2.behaviors.LoggableBehavior'
+                        'class' => 'LoggableBehavior'
                     )
             ));
 	}
